2443c73435191e130b63b168ee689f72abd18b93
esilberman <eve.silberman7390@gmail.com>
2025-05-31 11:11:14 -0400

    import

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,10 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Lexend:wght@160;240;320;380;460;800&display=swap",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
